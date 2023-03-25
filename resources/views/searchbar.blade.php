df7dff7c09bdcfe35040cc5f19d5f6be054af9c6
LuciaDunes <88556822+LuciaDunes@users.noreply.github.com>
2023-03-25 21:28:22 +0700

    tampilan doang, belum controller + web

---
+++ resources/views/searchbar.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css"></script>
+        <script srp="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+        <!-- jQuery -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+        <!-- Font Awesome -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></script>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="{{ asset('assets/css/searchbar.css') }}">
+    </head>
+
+    <body>
+    <div class="container">
+        <div class="row height d-flex justify-content-center align-items-center">
+            <div class="col-md-6">
+                <div class="form">
+                <i class="fa fa-search"></i>
+                <input type="text" 
+                    class="form-control form-input" 
+                    placeholder="Masukkan Keyword Agile">
+            <span class="left-pan"><i class="fa fa-microphone"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
